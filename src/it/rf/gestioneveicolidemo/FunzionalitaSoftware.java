d2eef1e8fd2f94fa435b67d709d01078fdb1ea8e
fabio <fabio@LAPTOP-64S77A3P.home-life.hub>
2026-02-19 14:56:19 +0100

    PROVA

--- src/it/rf/gestioneveicolidemo/FunzionalitaSoftware.java
+++ src/it/rf/gestioneveicolidemo/FunzionalitaSoftware.java
@@ -2,4 +2,9 @@ package it.rf.gestioneveicolidemo;
 
 public interface FunzionalitaSoftware {
 
+	public void caricamento(Veicolo []vettore);
+	public void stampa(Veicolo []vettore);
+	public Veicolo ricerca(Veicolo []vettore, String nTelaio);
+	public void modifica(Veicolo []vettore, Veicolo v);
+	
 }
